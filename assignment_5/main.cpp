efa45354851fbed0f03877d169c6feada36c198a
bootes16 <gjjohnson@gmail.com>
2018-04-14 08:02:39 +1000

    Initial Commit

---
+++ assignment_5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment_5
+//
+//  Created by Greg on 2018-04-14.
+//  Copyright © 2018 Greg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
